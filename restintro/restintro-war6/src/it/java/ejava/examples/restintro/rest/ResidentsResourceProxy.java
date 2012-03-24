747bfcfdc5772b655d6cfe5d88f18f27b1e62cb0
Jim Stafford <jcstaff@apl.jhu.edu>
2012-03-24 15:10:23 -0400

    adding UriBuilder instead of STr8ing.format

--- restintro/restintro-war6/src/it/java/ejava/examples/restintro/rest/ResidentsResourceProxy.java
+++ restintro/restintro-war6/src/it/java/ejava/examples/restintro/rest/ResidentsResourceProxy.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.UriBuilder;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -64,7 +65,9 @@ public class ResidentsResourceProxy implements DMVService {
 
     @Override
     public Resident createResident(Resident resident) {
-        String uri = String.format("%s/rest/%s/residents", serviceURI,implContext);
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents")
+                .build(implContext); 
         List<NameValuePair> params = RESTHelper.createArgsList();
         RESTHelper.add(params, "firstName", resident.getFirstName());
         RESTHelper.add(params, "lastName", resident.getLastName());
@@ -76,7 +79,7 @@ public class ResidentsResourceProxy implements DMVService {
             RESTHelper.add(params, "zip", info.getZip());
         }
         return doCheckCreateResult(
-                RESTHelper.postX(Resident.class, httpClient, uri, 
+                RESTHelper.postX(Resident.class, httpClient, uri.toString(), 
                 null, null, RESTHelper.toArray(params))).entity;
     }
 
@@ -88,9 +91,11 @@ public class ResidentsResourceProxy implements DMVService {
 
     @Override
     public List<Resident> getResidents(int start, int count) {
-        String uri = String.format("%s/rest/%s/residents", serviceURI,implContext);
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents")
+                .build(implContext); 
         return doCheckGetResult(
-            RESTHelper.getX(Residents.class, httpClient, uri, null, null,
+            RESTHelper.getX(Residents.class, httpClient, uri.toString(), null, null,
                 new BasicNameValuePair("start", new Integer(start).toString()),
                 new BasicNameValuePair("count", new Integer(count).toString())
                 )).entity;
@@ -98,15 +103,19 @@ public class ResidentsResourceProxy implements DMVService {
 
     @Override
     public Resident getResidentById(long id) {
-        String uri = String.format("%s/rest/%s/residents/%d", serviceURI, implContext, id);
-        return RESTHelper.getX(Resident.class, httpClient, uri, null, null).entity;
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents/{id}")
+                .build(implContext, id); 
+        return RESTHelper.getX(Resident.class, httpClient, uri.toString(), null, null).entity;
     }
 
     @Override
     public boolean updateResident(Resident resident) {
-        String uri = String.format("%s/rest/%s/residents/%d", serviceURI, implContext, resident.getId());
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents/{id}")
+                .build(implContext, resident.getId()); 
         Result<Void> result=doCheckPutResult(
-                RESTHelper.putXML(Void.class, httpClient, uri, null, resident));
+                RESTHelper.putXML(Void.class, httpClient, uri.toString(), null, resident));
         if (result.status >= 400) {
             log.debug("update failed {}:{}", result.status, result.errorMsg);
         }
@@ -115,23 +124,30 @@ public class ResidentsResourceProxy implements DMVService {
 
     @Override
     public int deleteResident(long id) {
-        String uri = String.format("%s/rest/%s/residents/%d", serviceURI,implContext, id);
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents/{id}")
+                .build(implContext, id); 
         return doCheckDeleteResult(
-            RESTHelper.deleteX(Integer.class, httpClient, uri, null, null)).entity;
+            RESTHelper.deleteX(Integer.class, httpClient, uri.toString(), null, null)).entity;
     }
 
     @Override
     public String getResidentNames() {
-        String uri = String.format("%s/rest/%s/residents/names", serviceURI,implContext);
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents/names")
+                .build(implContext); 
         return doCheckGetResult(
-            RESTHelper.getX(String.class, httpClient, uri, null, null)).entity;
+            RESTHelper.getX(String.class, httpClient, uri.toString(), null, null)).entity;
     }
 
     @Override
     public boolean isSamePerson(long p1, long p2) {
-        String uri = String.format("%s/rest/%s/residents/same", serviceURI,implContext);
+        URI uri=UriBuilder.fromUri(serviceURI)
+                .path("/rest/{implContext}/residents/same")
+                .build(implContext); 
+        //String uri = String.format("%s/rest/%s/residents/same", serviceURI,implContext);
         return doCheckGetResult(
-            RESTHelper.getX(Boolean.class, httpClient, uri, null, null,
+            RESTHelper.getX(Boolean.class, httpClient, uri.toString(), null, null,
                 new BasicNameValuePair("p1", new Long(p1).toString()),
                 new BasicNameValuePair("p2", new Long(p2).toString())
                 )).entity;
